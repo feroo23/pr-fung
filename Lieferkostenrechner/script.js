30095f40b8259e533c5c89b0fcb963fcba2db87d
Ferhat44 <fgumus1234@gmail.com>
2022-06-15 12:20:32 +0200

    test

--- Lieferkostenrechner/script.js
+++ Lieferkostenrechner/script.js
@@ -2,4 +2,7 @@ let lkrechnung = () =>{
     let waren = parseInt(document.getElementById(1))
     let ka = parseInt(document.getElementById(1))
 
+    if(isNaN(waren)){
+        ausgabe.value = 420
+    }
 }
